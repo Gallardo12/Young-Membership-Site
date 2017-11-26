39fa7173b10ea71dd0618d1c1626e8d140425058
Jose Gallardo <ing_gallardo@outlook.com>
2017-11-25 18:18:04 -0600

    Admins and Entrepreneur tables created

--- database/migrations/2017_11_25_234525_create_admins_table.php
+++ database/migrations/2017_11_25_234525_create_admins_table.php
@@ -4,18 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
+class CreateAdminsTable extends Migration {
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
 	public function up() {
-		Schema::create('users', function (Blueprint $table) {
+		Schema::create('admins', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('name');
 			$table->string('email')->unique();
 			$table->string('password');
+			$table->string('job_title');
 			$table->rememberToken();
 			$table->timestamps();
 		});
@@ -27,6 +28,6 @@ class CreateUsersTable extends Migration {
 	 * @return void
 	 */
 	public function down() {
-		Schema::dropIfExists('users');
+		Schema::dropIfExists('admins');
 	}
 }
