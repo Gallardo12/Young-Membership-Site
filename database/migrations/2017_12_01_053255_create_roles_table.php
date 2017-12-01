03e0bd87ed9d9c6eaa70487bb8576ca2c679bd98
Jose Gallardo <jgallardo@cadurango.mx>
2017-12-01 00:11:55 -0600

    Roles Model and migration added

--- database/migrations/2017_12_01_053255_create_roles_table.php
+++ database/migrations/2017_12_01_053255_create_roles_table.php
@@ -4,20 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminsTable extends Migration {
+class CreateRolesTable extends Migration {
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
 	public function up() {
-		Schema::create('admins', function (Blueprint $table) {
+		Schema::create('roles', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password');
-			$table->string('job_title');
-			$table->rememberToken();
 			$table->timestamps();
 		});
 	}
@@ -28,6 +24,6 @@ class CreateAdminsTable extends Migration {
 	 * @return void
 	 */
 	public function down() {
-		Schema::dropIfExists('admins');
+		Schema::dropIfExists('roles');
 	}
 }
